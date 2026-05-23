c618f34ab04185f6f7d52d856288ed155d77aa7c
Abir Hasan <abirhasan5208@gmail.com>
2026-05-23 19:05:55 +0600

    update controller

--- src/modules/issue/issue.controller.ts
+++ src/modules/issue/issue.controller.ts
@@ -1,63 +1,96 @@
 import type { Request, Response } from "express";
 import { issueService } from "./issue.service";
+import sendResponse from "../../utility/sendResponse";
 
 const createIssue = async (req: Request, res: Response) => {
   try {
     const reporter_id = req.user!.id;
 
     const result = await issueService.createIssueIntoDB(req.body, reporter_id);
-
-    res.status(201).json({
+    sendResponse(res, {
+      statusCode: 201,
       success: true,
       message: "Issue created successfully",
       data: result,
     });
+    // res.status(201).json({
+    //   success: true,
+    //   message: "Issue created successfully",
+    //   data: result,
+    // });
   } catch (error: unknown) {
     const errorMessage =
       error instanceof Error ? error.message : "Something went wrong";
-
-    res.status(500).json({
+    sendResponse(res, {
+      statusCode: 500,
       success: false,
       message: errorMessage,
       errors: errorMessage,
     });
+    // res.status(500).json({
+    //   success: false,
+    //   message: errorMessage,
+    //   errors: errorMessage,
+    // });
   }
 };
 
 const getAllIssues = async (req: Request, res: Response) => {
   try {
     const result = await issueService.getAllIssuesFromDB(req.query);
-
-    res.status(200).json({
+    sendResponse(res, {
+      statusCode: 200,
       success: true,
+      message: "Issue fetched successfully",
       data: result,
     });
+    // res.status(200).json({
+    //   success: true,
+    //   data: result,
+    // });
   } catch (error: unknown) {
     const errorMessage =
       error instanceof Error ? error.message : "Something went wrong";
-
-    res.status(500).json({
+    sendResponse(res, {
+      statusCode: 500,
       success: false,
       message: errorMessage,
+      errors: errorMessage,
     });
+    // res.status(500).json({
+    //   success: false,
+    //   message: errorMessage,
+    // });
   }
 };
 const getsingleIssue = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
     const result = await issueService.getSingleIssueFromDB(id as string);
-    res.status(200).json({
+
+    sendResponse(res, {
+      statusCode: 200,
       success: true,
+      message: "Issue get successfully",
       data: result,
     });
+    // res.status(200).json({
+    //   success: true,
+    //   data: result,
+    // });
   } catch (error: unknown) {
     const errorMessage =
       error instanceof Error ? error.message : "Something went wrong";
-
-    res.status(500).json({
+    sendResponse(res, {
+      statusCode: 500,
       success: false,
       message: errorMessage,
+      errors: errorMessage,
     });
+    // res.status(500).json({
+    //   success: false,
+    //   message: errorMessage,
+    // });
   }
 };
 
@@ -78,19 +111,31 @@ const updateIssue = async (req: Request, res: Response) => {
       },
     );
 
-    res.status(200).json({
+    sendResponse(res, {
+      statusCode: 200,
       success: true,
       message: "Issue updated successfully",
       data: result,
     });
+
+    // res.status(200).json({
+    //   success: true,
+    //   message: "Issue updated successfully",
+    //   data: result,
+    // });
   } catch (error: unknown) {
     const errorMessage =
       error instanceof Error ? error.message : "Something went wrong";
-
-    res.status(500).json({
+    sendResponse(res, {
+      statusCode: 500,
       success: false,
       message: errorMessage,
+      errors: errorMessage,
     });
+    // res.status(500).json({
+    //   success: false,
+    //   message: errorMessage,
+    // });
   }
 };
 
@@ -98,20 +143,29 @@ const deleteIssue = async (req: Request, res: Response) => {
   const { id } = req.params;
 
   try {
-    await issueService.deleteIssueFromDB(id as  string);
-
-    res.status(200).json({
+    await issueService.deleteIssueFromDB(id as string);
+    sendResponse(res, {
+      statusCode: 200,
       success: true,
       message: "Issue deleted successfully",
     });
+    // res.status(200).json({
+    //   success: true,
+    //   message: "Issue deleted successfully",
+    // });
   } catch (error: unknown) {
     const errorMessage =
       error instanceof Error ? error.message : "Something went wrong";
-
-    res.status(500).json({
+    sendResponse(res, {
+      statusCode: 500,
       success: false,
       message: errorMessage,
+      errors: errorMessage,
     });
+    // res.status(500).json({
+    //   success: false,
+    //   message: errorMessage,
+    // });
   }
 };
 
